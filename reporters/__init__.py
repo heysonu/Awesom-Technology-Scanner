ce635834218e76151f88adcd2a7c561f05cfed00
Rohit Mandal <89246146+heysonu@users.noreply.github.com>
2026-04-30 09:30:51 +0530

    Delete reporters directory

--- reporters/__init__.py
+++
@@ -1,5 +0,0 @@
-from .console import ConsoleReporter
-from .json_reporter import JSONReporter
-from .html_reporter import HTMLReporter
-
-__all__ = ['ConsoleReporter', 'JSONReporter', 'HTMLReporter']
